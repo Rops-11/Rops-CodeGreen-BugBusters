8031c8848a922c4a79452d598fe898a968ad33d5
Kenneth Joel Urbano <kenneth.urbano0127@gmail.com>
2025-05-16 23:06:09 +0800

    added login test for normal user

---
+++ cypress/e2e/user/signup.cy.ts
@@ -0,0 +1,36 @@
+/// <reference types="cypress" />
+
+describe("login", () => {
+  // Define test data for a normal user
+  // ** IMPORTANT: Replace with actual normal user credentials for testing **
+  const normalUserEmail = "thereal.ugly11@gmail.com"; // Updated email
+  const normalUserPassword = "123123123123"; // Updated password
+
+  describe("normal user", () => {
+    beforeEach(() => {
+      cy.visit("/login");
+    });
+
+    it("should allow a normal user to successfully log in and navigate through the pages and then logout", () => {
+      cy.get("#email").type(normalUserEmail);
+
+      cy.get("#password").type(normalUserPassword);
+
+      cy.get("button").contains("Login").click();
+
+      cy.url().should("include", "/homepage");
+
+      cy.contains("About").click().wait(3000);
+
+      cy.contains("Policies").click().wait(2000);
+      cy.contains("Protocol").click().wait(2000);
+      cy.contains("Policies").click().wait(2000);
+      cy.contains("Rules and Regulations").click().wait(2000);
+
+      cy.contains("Account").click();
+      cy.contains("Log Out").click();
+
+        cy.url().should("include", "/login");
+    });
+  });
+});
